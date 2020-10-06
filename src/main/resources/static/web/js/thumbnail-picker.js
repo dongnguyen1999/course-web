e1936a4c1e54bdfc5f6a0f2bf60b2449e1f9e9ce
Đông Nguyễn <nvdkg1999@gmail.com>
2020-10-06 22:10:08 +0700

    Complete new course form 95%

--- src/main/resources/static/web/js/thumbnail-picker.js
+++ src/main/resources/static/web/js/thumbnail-picker.js
@@ -1,6 +1,30 @@
-// function setThumbnailPicker() {
-//   let thumbnailImg = $('.img-thumbnail');
-//   thumbnailImg.mouseover(function () {
-//     thumbnailImg.src =
-//   })
-// }
+function setThumbnailPicker() {
+  let cameraIcon = $('#camera-icon');
+  $('#imgThumbnail')
+      .mouseenter(function () {
+        cameraIcon.removeClass('d-none');
+        cameraIcon.addClass('d-block');
+        $('#imgThumbnail').css({
+          "opacity": "0.5"
+        });
+        cameraIcon.css({
+          "position": "absolute",
+          "font-size": "10rem",
+          "left": "50%",
+          "top": "50%",
+          "transform": "translate(-50%, -50%)",
+        });
+      })
+      .mouseleave(function () {
+        cameraIcon.removeClass('d-block');
+        cameraIcon.addClass('d-none');
+        $('#imgThumbnail').css({
+          "opacity": "1"
+        });
+      })
+      .click(showPicker)
+}
+
+function showPicker() {
+  console.log('showPicker');
+}
